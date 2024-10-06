0fe8dfe8ccb2b901e77abe03e2fc55aca7c52fcf
yohannesjho <jhonians@outlook.com>
2024-10-07 01:41:11 +0300

    Add checkout button

--- front-end/my-app/app/cart/page.js
+++ front-end/my-app/app/cart/page.js
@@ -35,6 +35,7 @@ export default function CartPage() {
       </div>
       <div className='my-4 flex justify-between items-center space-x-8'>
         <p className='bg-yellow-500 px-4 py-2 rounded-md '> Total Price: ${totalPrice.toFixed(2)}</p>
+        <button className='bg-green-500 hover:bg-green-300 duration-300  px-4 py-2 mt-4 rounded-md text-white'>Proceed to checkout</button>
         <button onClick={clearCart} className="bg-blue-500 hover:bg-blue-300 duration-300 px-4 py-2 mt-4 rounded-md text-white">Clear Cart</button>
       </div>
     </div>
